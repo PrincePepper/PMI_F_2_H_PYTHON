0b1784c438a5eaf1fe8456ccce73ea657866aab0
PrincePepper <semyon3336@gmail.com>
2020-05-22 15:37:17 +1000

    #4 pack_tasks 13.04.20 -решена 1_libra.py

---
+++ 5pack_tasks/1_libra.py
@@ -0,0 +1,27 @@
+class Balance:
+    def __init__(self):
+        self.right = 0
+        self.left = 0
+
+    def add_left(self, item):
+        self.left = self.left + item
+
+    def add_right(self, item):
+        self.right = self.right + item
+
+    def result(self):
+        if self.left == self.right:
+            return '='
+        elif self.left > self.right:
+            return 'L'
+        else:
+            return 'R'
+
+
+balance = Balance()
+balance.add_right(10)
+balance.add_left(5)
+balance.add_left(5)
+print(balance.result())
+balance.add_left(1)
+print(balance.result())
