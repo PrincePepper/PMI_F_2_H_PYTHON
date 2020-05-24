3fb8ce23e5bdbccb2b60d3b2e2092da81d1c7b35
PrincePepper <semyon3336@gmail.com>
2020-05-24 19:16:30 +1000

    #4 pack_tasks 13.04.20 -решена 2_MyVector.py

---
+++ 5pack_tasks/2_MyVector.py
@@ -0,0 +1,51 @@
+import math
+
+
+class MyVector:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        return MyVector(self.x + other.x, self.y + other.y)
+
+    def __iadd__(self, other):
+        self.x += other.x
+        self.y += other.y
+        return self
+
+    def __sub__(self, other):
+        return MyVector(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other):
+        return MyVector(self.x * other, self.y * other)
+
+    def __imul__(self, other):
+        self.x -= other.x
+        self.y -= other.y
+        return self
+
+    def __rmul__(self, other):
+        return MyVector(self.x * other, self.y * other)
+
+    def __eq__(self, other):
+        if self.x == other.x1 and self.y == other.x2:
+            return True
+        return False
+
+    def __ne__(self, other):
+        if self.x != other.x1 and self.y != other.x2:
+            return True
+        return False
+
+    def __len__(self):
+        return math.floor(math.sqrt(self.x ** 2 + self.y ** 2))
+
+    def __str__(self):
+        return '({}, {})'.format(self.x, self.y)
+
+
+v1 = MyVector(-2, 5)
+v2 = MyVector(5, -4)
+v2 -= v1
+print(v2)
