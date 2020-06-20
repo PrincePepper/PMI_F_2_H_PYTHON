304ddb4226b8de3dee3b2e8d9d4f35bb8e98db14
PrincePepper <semyon3336@gmail.com>
2020-06-20 18:02:36 +1000

    #6pack_task -написаны классы фигур, проверку все прошли.

--- 6pack_tasks(chess_pieces)/main.py
+++ 6pack_tasks(chess_pieces)/main.py
@@ -35,7 +35,8 @@ def parse_coords(coords):
 
 
 class Board:
-    '''Класс доски  '''
+    """Класс доски  """
+
     def __init__(self):
         self.color = WHITE
         self.field = [[None] * 8 for row in range(8)]
@@ -116,6 +117,7 @@ class Piece:
 
 
 class Rook(Piece):
+    # Ладья ходит только по вертикали и только по диоганали
     def __init__(self, color):
         super().__init__(color)
         self.not_moved = True
@@ -151,6 +153,7 @@ class Rook(Piece):
 
 
 class Pawn(Piece):
+    # Пешка ходит на 2 клетки вперед при 1-м ходе или на 1 клетку всегда
     def char(self):
         return 'P'
 
@@ -178,35 +181,169 @@ class Pawn(Piece):
                 and row + 2 * direction == row1
                 and board.field[row + direction][col] is None):
             return True
+        return False
+
+    def can_attack(self, board, row, col, row1, col1):
+        if not (board.field[row1][col1] and board.field[row1][col1].color == opponent(self.color)):
+            return False
+        if self.color == WHITE and abs(col - col1) == row1 - row == 1:
+            return True
+        if self.color == BLACK and abs(col - col1) == row - row1 == 1:
+            return True
 
         return False
 
 
+class Knight(Piece):
+    # Конь может ходить буквой "Г"
+    def char(self):
+        return 'N'
+
+    def can_move(self, board, row, col, row1, col1):
+        # конь не может ходить по вертикали\горизонтали
+        if row == row1 or col == col1:
+            return False
+        if abs(row1 - row) + abs(col1 - col) == 3:
+            return True
+        return False
+
+    def can_attack(self, board, row, col, row1, col1):
+        return self.can_move(board, row, col, row1, col1)
+
+
+class Bishop(Piece):
+    # может ходить только по диагонали
+    def char(self):
+        return 'B'
+
+    def can_move(self, board, row, col, row1, col1):
+        # Слон не может ходить по вертикали\горизонтали
+        if row == row1 or col == col1:
+            return False
+
+        # в ходе по диагонали
+        # смещение по горизонтали == смещению по вертикали
+        if abs(row - row1) != abs(col - col1):
+            return False
+
+        step_row = 1 if (row1 >= row) else -1
+        step_col = 1 if (col1 >= col) else -1
+        for r, c in zip(range(row + step_row, row1, step_row), range(col + step_col, col1, step_col)):
+            # Если на пути по диагонали есть фигура
+            if board.get_piece(r, c) is not None:
+                return False
+        return True
+
+    def can_attack(self, board, row, col, row1, col1):
+        return self.can_move(board, row, col, row1, col1)
+
+
+class Queen(Piece):
+    # может ходить как по вертикали/горизонтали, так и по диагонали
+    def char(self):
+        return 'Q'
+
+    def can_move(self, board, row, col, row1, col1):
+        # клетки совпадают
+        if row == row1 and col == col1:
+            return False
+
+        # ход по вертикали
+        if col == col1:
+            step = 1 if (row1 >= row) else -1
+            for r in range(row + step, row1, step):
+                # Если на пути по вертикали есть фигура
+                if not (board.get_piece(r, col) is None):
+                    return False
+            return True
+
+        # ход по горизонтали
+        if row == row1:
+            step = 1 if (col1 >= col) else -1
+            for c in range(col + step, col1, step):
+                # Если на пути по горизонтали есть фигура
+                if not (board.get_piece(row, c) is None):
+                    return False
+            return True
+
+        # ход по диагонали
+        if row != row1 and col != col1:
+            # в ходе по диагонали
+            # смещение по горизонтали == смещению по вертикали
+            if abs(row - row1) != abs(col - col1):
+                return False
+
+            step_row = 1 if (row1 >= row) else -1
+            step_col = 1 if (col1 >= col) else -1
+            for r, c in zip(range(row + step_row, row1, step_row),
+                            range(col + step_col, col1, step_col)):
+                # Если на пути по диагонали есть фигура
+                if not (board.get_piece(r, c) is None):
+                    return False
+            return True
+        return False
+
+    def can_attack(self, board, row, col, row1, col1):
+        return self.can_move(board, row, col, row1, col1)
+
+
+class King(Piece):
+    # может сходить на 1 клетку вокруг себя
+    def char(self):
+        return 'K'
+
+    def can_move(self, board, row, col, row1, col1):
+        if abs(row - row1) > 1 or abs(col - col1) > 1:
+            return False
+        return True
+
+    def can_attack(self, board, row, col, row1, col1):
+        return self.can_move(board, row, col, row1, col1)
+
+
 def main():
-    # Создаём доску
+    row0 = 0
+    col0 = 3
+    knight = King(WHITE)
     board = Board()
-    # Цикл ввода команд игроков
-    while True:
-        # Выводим доску
-        print_board(board)
-        # Подсказка по командам
-        print('Команды:')
-        print('    exit                               -- выход')
-        print('    <coord1> <coord2>     -- ход из клетки (coord1) в клетку (coord2)')
+    for row in range(7, -1, -1):
+        for col in range(8):
+            if row == row0 and col == col0:
+                print(knight.char(), end='')
+            elif knight.can_move(board, row0, col0, row, col):
+                print('x', end='')
+            else:
+                print('-', end='')
+        print()
 
-        # Выводим чей ход
-        if board.current_player_color() == WHITE:
-            print('Ход белых: ', end='')
-        else:
-            print('Ход чёрных: ', end='')
+        # # Создаём доску
+        # board = Board()
+        # # Цикл ввода команд игроков
+        # while True:
+        #     # Выводим доску
+        #     print_board(board)
+        #     # Подсказка по командам
+        #     print('Команды:')
+        #     print('    exit                               -- выход')
+        #     print('    <coord1> <coord2>     -- ход из клетки (coord1) в клетку (coord2)')
+        #
+        #     # Выводим чей ход
+        #     if board.current_player_color() == WHITE:
+        #         print('Ход белых: ', end='')
+        #     else:
+        #         print('Ход чёрных: ', end='')
+        #
+        #     command = input()
+        #     if command == 'exit':
+        #         break
+        #
+        #     row, col, row1, col1 = parse_coords(command)
+        #
+        #     if board.move_piece(row, col, row1, col1):
+        #         print('Ход успешен')
+        #     else:
+        #         print('Координаты некорректы! Попробуйте другой ход!')
 
-        command = input()
-        if command == 'exit':
-            break
 
-        row, col, row1, col1 = parse_coords(command)
-
-        if board.move_piece(row, col, row1, col1):
-            print('Ход успешен')
-        else:
-            print('Координаты некорректы! Попробуйте другой ход!')
+if __name__ == '__main__':
+    main()
